9e5c67db18f7921ff82c7dc705dfce4f915abfbc
Zoran Lazarevic <lazarevic.zoki91@gmail.com>
2021-12-02 06:23:58 +0100

    day 2: part2

--- day-02/src/main.rs
+++ day-02/src/main.rs
@@ -6,34 +6,49 @@ fn main() {
     let mut submarine = Submarine::new();
     submarine.apply_commands(&input);
     println!("part 1 solution: {}", submarine.get_score());
+
+    submarine.reset();
+    submarine.apply_commands2(&input);
+    println!("part 2 solution: {}", submarine.get_score());
 }
 
 struct Submarine {
-    pos: (usize, usize),
+    pos: (usize, usize, usize),
 }
 impl Submarine {
     pub fn new() -> Self {
-        Self { pos: (0, 0) }
+        Self { pos: (0, 0, 0) }
     }
 
     pub fn apply_commands(&mut self, commands: &[Direction]) {
         for dir in commands {
             match dir {
+                Direction::Forward(val) => self.pos.0 += val,
+                Direction::Up(val) => self.pos.1 -= val,
+                Direction::Down(val) => self.pos.1 += val,
+            }
+        }
+    }
+
+    pub fn apply_commands2(&mut self, commands: &[Direction]) {
+        for dir in commands {
+            match dir {
                 Direction::Forward(val) => {
                     self.pos.0 += val;
+                    self.pos.1 += val * self.pos.2;
                 }
-                Direction::Up(val) => {
-                    self.pos.1 -= val;
-                }
-                Direction::Down(val) => {
-                    self.pos.1 += val;
-                }
+                Direction::Up(val) => self.pos.2 -= val,
+                Direction::Down(val) => self.pos.2 += val,
             }
         }
     }
 
     pub fn get_score(&self) -> usize {
         self.pos.0 * self.pos.1
+    }
+
+    pub fn reset(&mut self) {
+        self.pos = (0, 0, 0);
     }
 }
 
@@ -48,7 +63,7 @@ fn read(filename: &str) -> Vec<Direction> {
         .expect("Failed to read file")
         .lines()
         .map(|l| {
-            let mut items = l.split_terminator(" ");
+            let mut items = l.split_terminator(' ');
             let dir = items.next().unwrap();
             let val = items.next().unwrap().parse().unwrap();
             match dir {
@@ -63,10 +78,23 @@ fn read(filename: &str) -> Vec<Direction> {
 
 #[cfg(test)]
 mod tests {
-    use crate::read;
+    use crate::{read, Submarine};
 
     #[test]
     fn part1_test() {
-        assert_eq!(2, 1);
+        let input = read("test-input.txt");
+        let mut submarine = Submarine::new();
+        submarine.apply_commands(&input);
+
+        assert_eq!(submarine.get_score(), 150);
+    }
+
+    #[test]
+    fn part2_test() {
+        let input = read("test-input.txt");
+        let mut submarine = Submarine::new();
+        submarine.apply_commands2(&input);
+
+        assert_eq!(submarine.get_score(), 900);
     }
 }

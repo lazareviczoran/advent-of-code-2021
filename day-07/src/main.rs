84e0cf502ab6ffb226576d0c57c7e1b7c41967ee
Zoran Lazarevic <lazarevic.zoki91@gmail.com>
2021-12-07 07:19:44 +0100

    day 7: part 2

--- day-07/src/main.rs
+++ day-07/src/main.rs
@@ -2,36 +2,39 @@ use std::fs::read_to_string;
 
 fn main() {
     let input = read("input.txt");
-    println!("part1 solution: {}", calculate_min_fuel(&input));
+    println!("part1 solution: {:?}", calc_min_fuel_single_step(&input));
+    println!("part2 solution: {:?}", calc_min_fuel_multi_step(&input));
 }
 
-fn calculate_min_fuel(positions: &[usize]) -> usize {
-    let mut min_consumption = usize::MAX;
-    for target_pos in 0..positions.len() {
-        let fuel_consumption = calculate_fuel_consumption_for_target_pos(positions, &target_pos);
-        if fuel_consumption < min_consumption {
-            min_consumption = fuel_consumption;
-        }
-    }
-
-    // println!("{:?}", most_common);
-    min_consumption
+fn calc_min_fuel_single_step(positions: &[usize]) -> Option<usize> {
+    calc_min_fuel(positions, |x| x)
 }
 
-fn calculate_fuel_consumption_for_target_pos(positions: &[usize], target: &usize) -> usize {
+fn calc_min_fuel_multi_step(positions: &[usize]) -> Option<usize> {
+    calc_min_fuel(positions, |x| (1..=x).sum())
+}
+
+fn calc_min_fuel<F>(positions: &[usize], step_fn: F) -> Option<usize>
+where
+    F: FnMut(usize) -> usize + Copy,
+{
+    (0..positions.len())
+        .map(|target_pos| calc_fuel_consumption_for_target_pos(positions, &target_pos, step_fn))
+        .min()
+}
+
+fn calc_fuel_consumption_for_target_pos<F>(
+    positions: &[usize],
+    target: &usize,
+    mut step_fn: F,
+) -> usize
+where
+    F: FnMut(usize) -> usize + Copy,
+{
+    let calc_required_fuel = |pos: &usize, target: &usize| pos.max(target) - pos.min(target);
     positions
         .iter()
-        .map(|pos| {
-            if pos != target {
-                if pos > target {
-                    *pos - target
-                } else {
-                    target - *pos
-                }
-            } else {
-                0
-            }
-        })
+        .map(|pos| step_fn(calc_required_fuel(pos, target)))
         .sum()
 }
 
@@ -50,11 +53,17 @@ fn read(filename: &str) -> Vec<usize> {
 
 #[cfg(test)]
 mod tests {
-    use crate::{calculate_min_fuel, read};
+    use crate::{calc_min_fuel_multi_step, calc_min_fuel_single_step, read};
 
     #[test]
     fn part1_test() {
         let input = read("test-input.txt");
-        assert_eq!(calculate_min_fuel(&input), 37);
+        assert_eq!(calc_min_fuel_single_step(&input), Some(37));
+    }
+
+    #[test]
+    fn part2_test() {
+        let input = read("test-input.txt");
+        assert_eq!(calc_min_fuel_multi_step(&input), Some(168));
     }
 }

f0ea2aaca007df513c14300da0845445b4fc998b
Zoran Lazarevic <lazarevic.zoki91@gmail.com>
2021-12-07 08:16:56 +0100

    day 7: switch usize to isize and use abs()

--- day-07/src/main.rs
+++ day-07/src/main.rs
@@ -6,33 +6,33 @@ fn main() {
     println!("part2 solution: {:?}", calc_min_fuel_multi_step(&input));
 }
 
-fn calc_min_fuel_single_step(positions: &[usize]) -> Option<usize> {
+fn calc_min_fuel_single_step(positions: &[isize]) -> Option<isize> {
     calc_min_fuel(positions, |x| x)
 }
 
-fn calc_min_fuel_multi_step(positions: &[usize]) -> Option<usize> {
+fn calc_min_fuel_multi_step(positions: &[isize]) -> Option<isize> {
     calc_min_fuel(positions, |x| (1..=x).sum())
 }
 
-fn calc_min_fuel(positions: &[usize], step_fn: impl Fn(usize) -> usize) -> Option<usize> {
-    (0..positions.len())
+fn calc_min_fuel(positions: &[isize], step_fn: impl Fn(isize) -> isize) -> Option<isize> {
+    (0..positions.len() as isize)
         .map(|target_pos| calc_fuel_consumption_for_target_pos(positions, &target_pos, &step_fn))
         .min()
 }
 
 fn calc_fuel_consumption_for_target_pos(
-    positions: &[usize],
-    target: &usize,
-    step_fn: impl Fn(usize) -> usize,
-) -> usize {
-    let calc_required_fuel = |pos: &usize, target: &usize| pos.max(target) - pos.min(target);
+    positions: &[isize],
+    target: &isize,
+    step_fn: impl Fn(isize) -> isize,
+) -> isize {
+    let calc_required_fuel = |pos: &isize, target: &isize| (pos - target).abs();
     positions
         .iter()
         .map(|pos| step_fn(calc_required_fuel(pos, target)))
         .sum()
 }
 
-fn read(filename: &str) -> Vec<usize> {
+fn read(filename: &str) -> Vec<isize> {
     read_to_string(filename)
         .expect("Failed to read file")
         .lines()

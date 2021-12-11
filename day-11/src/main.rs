141bf3a3078b280e7aaf04c38277cc20e19dc1c1
Zoran Lazarevic <lazarevic.zoki91@gmail.com>
2021-12-11 06:42:08 +0100

    day 11: part 2

--- day-11/src/main.rs
+++ day-11/src/main.rs
@@ -1,14 +1,31 @@
 use std::{collections::HashSet, fs::read_to_string};
 
 fn main() {
-    let map = read("input.txt");
+    let mut map = read("input.txt");
     println!(
         "part1 solution: {}",
-        get_score_after_n_steps(&mut map.clone(), 100)
+        score_after_n_steps(&mut map.clone(), 100)
     );
+    println!("part2 solution: {}", find_first_sync_step(&mut map));
 }
 
-fn get_score_after_n_steps(map: &mut Vec<Vec<usize>>, steps: usize) -> usize {
+fn find_first_sync_step(map: &mut Vec<Vec<usize>>) -> usize {
+    let mut has_flashed = HashSet::new();
+    for step in 1.. {
+        for y in 0..map.len() {
+            for x in 0..map[0].len() {
+                increase_item(map, (y as isize, x as isize), &mut has_flashed);
+            }
+        }
+        if has_flashed.len() == map.len() * map[0].len() {
+            return step;
+        }
+        has_flashed.clear();
+    }
+    unreachable!()
+}
+
+fn score_after_n_steps(map: &mut Vec<Vec<usize>>, steps: usize) -> usize {
     let mut count = 0;
     let mut has_flashed = HashSet::new();
     for _step in 0..steps {
@@ -18,7 +35,6 @@ fn get_score_after_n_steps(map: &mut Vec<Vec<usize>>, steps: usize) -> usize {
             }
         }
         has_flashed.clear();
-        print_map(map);
     }
     count
 }
@@ -38,9 +54,9 @@ fn increase_item(
     }
     map[y as usize][x as usize] += 1;
     let mut count = 0;
-    has_flashed.insert((y, x));
     if map[y as usize][x as usize] > 9 {
         map[y as usize][x as usize] = 0;
+        has_flashed.insert((y, x));
         count += 1;
         count += increase_item(map, (y - 1, x - 1), has_flashed);
         count += increase_item(map, (y - 1, x), has_flashed);
@@ -54,17 +70,6 @@ fn increase_item(
     count
 }
 
-fn print_map(map: &mut Vec<Vec<usize>>) {
-    let mut s = String::new();
-    for y in 0..map.len() {
-        for x in 0..map[0].len() {
-            s.push((map[y][x] as u8 + b'0') as char);
-        }
-        s.push('\n');
-    }
-    println!("{}", s);
-}
-
 fn read(filename: &str) -> Vec<Vec<usize>> {
     read_to_string(filename)
         .expect("Failed to read file")
@@ -75,12 +80,18 @@ fn read(filename: &str) -> Vec<Vec<usize>> {
 
 #[cfg(test)]
 mod tests {
-    use crate::{get_score_after_n_steps, read};
+    use crate::{find_first_sync_step, read, score_after_n_steps};
 
     #[test]
     fn part1_test() {
-        let map = read("test-input.txt");
-        assert_eq!(get_score_after_n_steps(&mut map.clone(), 10), 204);
-        assert_eq!(get_score_after_n_steps(&mut map.clone(), 100), 1656);
+        let mut map = read("test-input.txt");
+        assert_eq!(score_after_n_steps(&mut map.clone(), 10), 204);
+        assert_eq!(score_after_n_steps(&mut map, 100), 1656);
+    }
+
+    #[test]
+    fn part2_test() {
+        let mut map = read("test-input.txt");
+        assert_eq!(find_first_sync_step(&mut map), 195);
     }
 }

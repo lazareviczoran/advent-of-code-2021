5d2d854cadcada28be59449500373cc0c0f60361
Zoran Lazarevic <lazarevic.zoki91@gmail.com>
2021-12-12 06:40:30 +0100

    day 12: part 2

--- day-12/src/main.rs
+++ day-12/src/main.rs
@@ -5,27 +5,37 @@ use std::{
 
 fn main() {
     let connections = read("input.txt");
-    println!("part1 solution: {:?}", count_paths(&connections));
+    println!("part1 solution: {:?}", count_paths(&connections, false));
+    println!("part2 solution: {:?}", count_paths(&connections, true));
 }
 
-fn count_paths(connections: &HashMap<String, Vec<String>>) -> Option<usize> {
-    let mut q = vec![(String::from("start"), HashSet::new())];
+fn count_paths(
+    connections: &HashMap<String, Vec<String>>,
+    can_visit_one_small_twice: bool,
+) -> Option<usize> {
+    let mut q = vec![(String::from("start"), HashSet::new(), false)];
     let mut count = 0;
     while !q.is_empty() {
-        let (current, mut visited) = q.pop()?;
+        let (current, mut visited, mut visited_twice) = q.pop()?;
         if &current == "end" {
             count += 1;
             continue;
         }
         if current.chars().next()?.is_lowercase() {
-            if visited.contains(&current) {
+            let already_visited = visited.contains(&current);
+            if already_visited
+                && (!can_visit_one_small_twice || visited_twice || &current == "start")
+            {
                 continue;
+            }
+            if already_visited {
+                visited_twice = true;
             }
             visited.insert(current.clone());
         }
         let neighbours = connections.get(&current)?;
         for neighbour in neighbours {
-            q.push((neighbour.to_string(), visited.clone()));
+            q.push((neighbour.to_string(), visited.clone(), visited_twice));
         }
     }
 
@@ -56,18 +66,36 @@ mod tests {
     #[test]
     fn part1_test1() {
         let connections = read("test-input.txt");
-        assert_eq!(count_paths(&connections), Some(10));
+        assert_eq!(count_paths(&connections, false), Some(10));
     }
 
     #[test]
     fn part1_test2() {
         let connections = read("test-input2.txt");
-        assert_eq!(count_paths(&connections), Some(19));
+        assert_eq!(count_paths(&connections, false), Some(19));
     }
 
     #[test]
     fn part1_test3() {
         let connections = read("test-input3.txt");
-        assert_eq!(count_paths(&connections), Some(226));
+        assert_eq!(count_paths(&connections, false), Some(226));
+    }
+
+    #[test]
+    fn part2_test1() {
+        let connections = read("test-input.txt");
+        assert_eq!(count_paths(&connections, true), Some(36));
+    }
+
+    #[test]
+    fn part2_test2() {
+        let connections = read("test-input2.txt");
+        assert_eq!(count_paths(&connections, true), Some(103));
+    }
+
+    #[test]
+    fn part2_test3() {
+        let connections = read("test-input3.txt");
+        assert_eq!(count_paths(&connections, true), Some(3509));
     }
 }

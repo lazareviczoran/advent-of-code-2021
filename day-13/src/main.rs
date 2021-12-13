efb2ab9fcbe429aa86882a77fd9a207f0cb5389a
Zoran Lazarevic <lazarevic.zoki91@gmail.com>
2021-12-13 08:06:22 +0100

    day 13: fix coordinate positions

--- day-13/src/main.rs
+++ day-13/src/main.rs
@@ -20,7 +20,7 @@ fn fold_all(map: &HashSet<Point>, instructions: &[FoldInstr]) -> HashSet<Point> 
 fn fold(map: &HashSet<Point>, (fold_by_axis, fold_position): FoldInstr) -> HashSet<Point> {
     map.iter()
         .filter_map(|&(curr_x, curr_y)| match fold_by_axis {
-            'y' => match curr_x {
+            'x' => match curr_x {
                 x if x == fold_position => None,
                 x if x < fold_position => Some((x, curr_y)),
                 _ => Some((fold_position - (curr_x - fold_position), curr_y)),
@@ -38,10 +38,10 @@ fn print_map(map: &HashSet<Point>) {
     let (width, height) = map
         .iter()
         .fold((0, 0), |acc, &(x, y)| (acc.0.max(x), acc.1.max(y)));
-    let s = (0..=width)
-        .map(|x| {
-            (0..=height)
-                .map(|y| if map.contains(&(x, y)) { '#' } else { '.' })
+    let s = (0..=height)
+        .map(|y| {
+            (0..=width)
+                .map(|x| if map.contains(&(x, y)) { '#' } else { '.' })
                 .collect::<String>()
         })
         .collect::<Vec<_>>();
@@ -57,7 +57,7 @@ fn read(filename: &str) -> Option<(HashSet<Point>, Vec<FoldInstr>)> {
                 .lines()
                 .filter_map(|l| {
                     l.split_once(',')
-                        .and_then(|(x, y)| Some((y.parse().ok()?, x.parse().ok()?)))
+                        .and_then(|(x, y)| Some((x.parse().ok()?, y.parse().ok()?)))
                 })
                 .collect();
             let instr = instructions

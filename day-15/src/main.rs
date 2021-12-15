3fd9a1af01372bd39c374a0f7c4484e770baca10
Zoran Lazarevic <lazarevic.zoki91@gmail.com>
2021-12-15 10:07:56 +0100

    day 15: part 2

--- day-15/src/main.rs
+++ day-15/src/main.rs
@@ -2,7 +2,11 @@ use std::{cmp::Ordering, collections::BinaryHeap, fs::read_to_string};
 
 fn main() {
     let map = read("input.txt");
-    println!("part1 solution: {:?}", shortest_path(&map, (0, 0),));
+    println!("part1 solution: {:?}", shortest_path(&map));
+    println!(
+        "part2 solution: {:?}",
+        shortest_path(&build_large_map(&map))
+    );
 }
 
 #[derive(Copy, Clone, Eq, PartialEq)]
@@ -24,7 +28,8 @@ impl PartialOrd for State {
     }
 }
 
-fn shortest_path(map: &[Vec<usize>], start: (isize, isize)) -> Option<usize> {
+fn shortest_path(map: &[Vec<usize>]) -> Option<usize> {
+    let start = (0, 0);
     let target = (map[0].len() as isize - 1, map.len() as isize - 1);
     let mut dist = vec![vec![usize::MAX; map[0].len()]; map.len()];
 
@@ -46,7 +51,7 @@ fn shortest_path(map: &[Vec<usize>], start: (isize, isize)) -> Option<usize> {
             continue;
         }
 
-        for (next_x, next_y) in [(x - 1, y), (x + 1, y), (x, y - 1), (x, y + 1)]
+        [(x - 1, y), (x + 1, y), (x, y - 1), (x, y + 1)]
             .into_iter()
             .filter(|&(next_x, next_y)| {
                 next_x >= 0
@@ -54,20 +59,41 @@ fn shortest_path(map: &[Vec<usize>], start: (isize, isize)) -> Option<usize> {
                     && next_y >= 0
                     && (next_y as usize) < map.len()
             })
-        {
-            let next = State {
-                cost: cost + map[next_y as usize][next_x as usize],
-                position: (next_x, next_y),
-            };
+            .for_each(|(next_x, next_y)| {
+                let next = State {
+                    cost: cost + map[next_y as usize][next_x as usize],
+                    position: (next_x, next_y),
+                };
 
-            if next.cost < dist[next_y as usize][next_x as usize] {
-                heap.push(next);
-                dist[next_y as usize][next_x as usize] = next.cost;
+                if next.cost < dist[next_y as usize][next_x as usize] {
+                    heap.push(next);
+                    dist[next_y as usize][next_x as usize] = next.cost;
+                }
+            });
+    }
+
+    None
+}
+
+fn build_large_map(map: &[Vec<usize>]) -> Vec<Vec<usize>> {
+    let (tile_width, tile_height) = (map[0].len(), map.len());
+    let (full_width, full_height) = (5 * tile_width, 5 * tile_height);
+    let mut large_map = vec![vec![0; full_width]; full_height];
+    for y in 0..full_height {
+        for x in 0..full_width {
+            let current_block = (x / tile_width, y / tile_height);
+            match current_block {
+                (0, 0) => large_map[y][x] = map[y][x],
+                (_, 0) => large_map[y][x] = large_map[y][x - tile_width] + 1,
+                _ => large_map[y][x] = large_map[y - tile_height][x] + 1,
+            }
+            if large_map[y][x] == 10 {
+                large_map[y][x] = 1;
             }
         }
     }
 
-    None
+    large_map
 }
 
 fn read(filename: &str) -> Vec<Vec<usize>> {
@@ -80,11 +106,18 @@ fn read(filename: &str) -> Vec<Vec<usize>> {
 
 #[cfg(test)]
 mod tests {
-    use crate::{read, shortest_path};
+    use crate::{build_large_map, read, shortest_path};
 
     #[test]
     fn part1_test() {
         let map = read("test-input.txt");
-        assert_eq!(shortest_path(&map, (0, 0)), Some(40));
+        assert_eq!(shortest_path(&map), Some(40));
+    }
+
+    #[test]
+    fn part2_test() {
+        let map = read("test-input.txt");
+        let map = build_large_map(&map);
+        assert_eq!(shortest_path(&map), Some(315));
     }
 }
